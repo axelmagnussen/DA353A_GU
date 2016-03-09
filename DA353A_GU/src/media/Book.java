918ea1d792b5185d0025182bdf2c1c20ff48d451
Axel <Axel@axel_shoulder>
2016-03-09 17:13:16 +0100

    Book

---
+++ DA353A_GU/src/media/Book.java
@@ -0,0 +1,12 @@
+package media;
+
+public class Book extends Media{
+	
+	public Book(int id, String author, String title, int year, boolean isBorrowed){
+		
+		super(id, title, year, isBorrowed);
+	}
+	
+	
+
+}
