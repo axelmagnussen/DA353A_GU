9193163e290911e1cc43741729e3c7e862a0b8af
Axel <Axel@axel_shoulder>
2016-03-10 13:25:47 +0100

    asd

--- DA353A_GU/src/library/LibraryMember.java
+++ DA353A_GU/src/library/LibraryMember.java
@@ -1,9 +1,20 @@
 package library;
 
+import java.util.Date;
+
+import collections.AVLTree;
+import media.Media;
+
 public class LibraryMember {
 
-	public LibraryMember(String id, String name, String phone) {
-		// TODO Auto-generated constructor stub
+	private String id;
+
+	public LibraryMember(String id, String name, String phone, AVLTree<Date,Media>borrowedMedia ) {
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.borrowedMedia = borrowedMedia;
+		
 	}
 
 }
