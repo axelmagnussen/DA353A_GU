e9dfb45404308b21b475c1620aea2412f8537ded
John Doe <me@example.com>
2016-03-11 18:39:52 +0100

    Förslag till felhantering

--- DA353A_GU/src/library/UserPage.java
+++ DA353A_GU/src/library/UserPage.java
@@ -12,56 +12,71 @@ import javax.swing.JTextField;
 @SuppressWarnings("serial")
 public class UserPage extends JPanel implements ActionListener {
 
-	private LibraryController libraryController;
-	private JLabel welcomeXlbl = new JLabel();
-	private JLabel txtFlbl = new JLabel("Media ID:");
-	private Font welcomeFont = new Font("SansSerif", Font.PLAIN, 16);
-	private Font thisFont = new Font("SansSerif", Font.PLAIN, 14);
-	public JTextField mediaIDTxtF = new JTextField();
-	public JButton borrowBtn = new JButton("Borrow");
-	public JButton returnBtn = new JButton("Return");
-	public JButton signOutBtn = new JButton("Sign Out");
+    private LibraryController libraryController;
+    private JLabel welcomeXlbl = new JLabel();
+    private JLabel txtFlbl = new JLabel("Media ID:");
+    private Font welcomeFont = new Font("SansSerif", Font.PLAIN, 16);
+    private Font thisFont = new Font("SansSerif", Font.PLAIN, 14);
+    public JTextField mediaIDTxtF = new JTextField();
+    public JButton borrowBtn = new JButton("Borrow");
+    public JButton returnBtn = new JButton("Return");
+    public JButton signOutBtn = new JButton("Sign Out");
 
-	public UserPage(LibraryController libC) {
+    public UserPage(LibraryController libC) {
 
-		this.libraryController = libC;
+        this.libraryController = libC;
 
-		setLayout(null);
+        setLayout(null);
 
-		String message = libraryController.getCurrentUser().getName();
-		welcomeXlbl.setText("Welcome " + message + "!");
-		welcomeXlbl.setFont(welcomeFont);
-		welcomeXlbl.setBounds(110, 20, 100, 25);
-		welcomeXlbl.setSize(400, 20);
-		add(welcomeXlbl);
+        String message = libraryController.getCurrentUser().getName();
+        welcomeXlbl.setText("Welcome " + message + "!");
+        welcomeXlbl.setFont(welcomeFont);
+        welcomeXlbl.setBounds(110, 20, 100, 25);
+        welcomeXlbl.setSize(400, 20);
+        add(welcomeXlbl);
 
-		txtFlbl.setFont(thisFont);
-		txtFlbl.setBounds(100, 70, 80, 25);
-		txtFlbl.setSize(60, 20);
-		add(txtFlbl);
+        txtFlbl.setFont(thisFont);
+        txtFlbl.setBounds(100, 70, 80, 25);
+        txtFlbl.setSize(60, 20);
+        add(txtFlbl);
 
-		mediaIDTxtF.setBounds(170, 70, 140, 25);
-		add(mediaIDTxtF);
+        mediaIDTxtF.setBounds(170, 70, 140, 25);
+        add(mediaIDTxtF);
 
-		borrowBtn.setBounds(50, 120, 90, 25);
-		borrowBtn.setSize(90, 35);
-		add(borrowBtn);
-		borrowBtn.addActionListener(e -> {
-			libraryController.borrow(Integer.parseInt(mediaIDTxtF.getText()));
-		});
+        borrowBtn.setBounds(50, 120, 90, 25);
+        borrowBtn.setSize(90, 35);
+        add(borrowBtn);
+        borrowBtn.addActionListener(e -> {
+            // /*
+            // * Lägg till en try-catch om Integer.parseInt får fnatt?
+            // */
+            // try {
+            // String str = mediaIDTxtF.getText();
+            // if (str == null) {
+            // throw new NullPointerException();
+            // }
+            // Integer mediaId = Integer.parseInt(str);
+            // libraryController.borrow(mediaId);
+            // } catch (NumberFormatException except) {
+            // // Hjälpfullt meddelande i JOptionPane?
+            // } catch (NullPointerException except) {
+            // // Mer hjälpfulla meddelande?
+            // }
+            libraryController.borrow(Integer.parseInt(mediaIDTxtF.getText()));
+        });
 
-		returnBtn.setBounds(160, 120, 90, 35);
-		returnBtn.setSize(90, 35);
-		add(returnBtn);
-		returnBtn.addActionListener(this);
+        returnBtn.setBounds(160, 120, 90, 35);
+        returnBtn.setSize(90, 35);
+        add(returnBtn);
+        returnBtn.addActionListener(this);
 
-		signOutBtn.setBounds(270, 120, 90, 35);
-		signOutBtn.setSize(90, 35);
-		add(signOutBtn);
-		signOutBtn.addActionListener(this);
-	}
+        signOutBtn.setBounds(270, 120, 90, 35);
+        signOutBtn.setSize(90, 35);
+        add(signOutBtn);
+        signOutBtn.addActionListener(this);
+    }
 
-	public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {
 
-	}
+    }
 }
