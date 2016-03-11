ba912ae7a573fee3f94a919222969b9fe38c66c6
John Doe <me@example.com>
2016-03-11 11:33:36 +0100

    La till fält

--- DA353A_GU/src/library/LibraryMember.java
+++ DA353A_GU/src/library/LibraryMember.java
@@ -8,13 +8,16 @@ import media.Media;
 public class LibraryMember {
 
 	private String id;
+	private String name;
+	private String phone;
+	private AVLTree<Date, Media> borrowedMedia;
 
-	public LibraryMember(String id, String name, String phone, AVLTree<Date,Media>borrowedMedia ) {
+	public LibraryMember(String id, String name, String phone, AVLTree<Date, Media> borrowedMedia) {
 		this.id = id;
 		this.name = name;
 		this.phone = phone;
 		this.borrowedMedia = borrowedMedia;
-		
+
 	}
 
 }
