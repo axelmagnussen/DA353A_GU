7370bad9bfff68dd6bb797ca3ed0d9f870067124
John Doe <me@example.com>
2016-03-11 14:19:28 +0100

    jaja

--- DA353A_GU/src/library/LibraryController.java
+++ DA353A_GU/src/library/LibraryController.java
@@ -2,34 +2,36 @@ package library;
 
 import javax.swing.JOptionPane;
 
-public class LibraryController 
-{
+public class LibraryController {
 	private Library library;
 	private LibraryMember currentUser;
 
 	public LibraryController() {
 		library = new Library("src/files/Lantagare.txt", "src/files/Media.txt");
 	}
-	
+
 	public void setUserInput(String idNbr) {
-		if(library.memberExists(idNbr)) 
-		{
+		if (library.memberExists(idNbr)) {
 			this.setCurrentUser(library.getMember(idNbr));
-			
+
 			JOptionPane.showMessageDialog(null, "User does exist :)", "Info", JOptionPane.INFORMATION_MESSAGE);
 			LibraryApp.showUserPage();
-		}
-		else 
-		{
-			JOptionPane.showMessageDialog(null, "User does not exist! Contact staff or try again.", "Error", JOptionPane.ERROR_MESSAGE);
+		} else {
+			JOptionPane.showMessageDialog(null, "User does not exist! Contact staff or try again.", "Error",
+					JOptionPane.ERROR_MESSAGE);
 		}
 	}
-	
+
 	public void setCurrentUser(LibraryMember currentUser) {
 		this.currentUser = currentUser;
 	}
 
 	public LibraryMember getCurrentUser() {
 		return this.currentUser;
+	}
+
+	public void search(int parseInt) {
+		System.out.println(parseInt);
+
 	}
 }
--- DA353A_GU/src/library/UserPage.java
+++ DA353A_GU/src/library/UserPage.java
@@ -11,58 +11,57 @@ import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
 public class UserPage extends JPanel implements ActionListener {
-	
+
 	private LibraryController libraryController;
 	private JLabel welcomeXlbl = new JLabel();
 	private JLabel txtFlbl = new JLabel("Media ID:");
-	private Font welcomeFont = new Font ("SansSerif", Font.PLAIN, 16);
-	private Font thisFont = new Font ("SansSerif", Font.PLAIN, 14);
+	private Font welcomeFont = new Font("SansSerif", Font.PLAIN, 16);
+	private Font thisFont = new Font("SansSerif", Font.PLAIN, 14);
 	public JTextField mediaIDTxtF = new JTextField();
 	public JButton borrowBtn = new JButton("Borrow");
 	public JButton returnBtn = new JButton("Return");
 	public JButton signOutBtn = new JButton("Sign Out");
-	
+
 	public UserPage(LibraryController libC) {
-		
+
 		this.libraryController = libC;
-		
+
 		setLayout(null);
-		
+
 		String message = libraryController.getCurrentUser().getName();
 		welcomeXlbl.setText("Welcome " + message + "!");
 		welcomeXlbl.setFont(welcomeFont);
 		welcomeXlbl.setBounds(110, 20, 100, 25);
-		welcomeXlbl.setSize(400,20);
+		welcomeXlbl.setSize(400, 20);
 		add(welcomeXlbl);
-		
+
 		txtFlbl.setFont(thisFont);
-		txtFlbl.setBounds(100,70,80,25);
-		txtFlbl.setSize(60,20);
+		txtFlbl.setBounds(100, 70, 80, 25);
+		txtFlbl.setSize(60, 20);
 		add(txtFlbl);
-		
-		mediaIDTxtF.setBounds(170,70,140,25);
+
+		mediaIDTxtF.setBounds(170, 70, 140, 25);
 		add(mediaIDTxtF);
-		
+
 		borrowBtn.setBounds(50, 120, 90, 25);
 		borrowBtn.setSize(90, 35);
 		add(borrowBtn);
-		borrowBtn.addActionListener(this);
-		
-		
-		returnBtn.setBounds(160,120, 90, 35);
+		borrowBtn.addActionListener(e -> {
+			libraryController.search(Integer.parseInt(mediaIDTxtF.getText()));
+		});
+
+		returnBtn.setBounds(160, 120, 90, 35);
 		returnBtn.setSize(90, 35);
 		add(returnBtn);
 		returnBtn.addActionListener(this);
-				
+
 		signOutBtn.setBounds(270, 120, 90, 35);
 		signOutBtn.setSize(90, 35);
 		add(signOutBtn);
 		signOutBtn.addActionListener(this);
 	}
 
-	
 	public void actionPerformed(ActionEvent e) {
-		
-		
+
 	}
 }
