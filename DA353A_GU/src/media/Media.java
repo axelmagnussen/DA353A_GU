ae57eabd9c056b9ac3bc4cff082b985d65e5c10e
Jonas <jonazeiselt@gmail.com>
2016-03-11 12:03:40 +0100

    add to media

--- DA353A_GU/src/media/Media.java
+++ DA353A_GU/src/media/Media.java
@@ -1,11 +1,14 @@
 package media;
 
+import java.util.Date;
+
 public class Media {
 	
 	private int id;
 	private boolean isBorrowed;
 	private int year;
 	private String title;
+	private Date borrowDate;
 
 	public Media(int id, String title, int year, boolean isBorrowed) 
 	{
@@ -33,5 +36,22 @@ public class Media {
 	public boolean isBorrowed() 
 	{
 		return isBorrowed; 
+	}
+	
+	public void setBorrowDate(Date borrowDate)
+	{
+		this.borrowDate = borrowDate;
+	}
+	
+	public Date getBorrowDate()
+	{
+		return this.borrowDate;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return "Media [id=" + id + ", isBorrowed=" + isBorrowed + ", year=" + year + ", title=" + title
+				+ ", borrowDate=" + borrowDate + "]";
 	}
 }
