57546263323a36a599af99b38795a178a8d19537
John Doe <me@example.com>
2016-03-11 18:43:55 +0100

    Gjorde den abstrakt så den inte går att instansiera

--- DA353A_GU/src/media/Media.java
+++ DA353A_GU/src/media/Media.java
@@ -2,56 +2,48 @@ package media;
 
 import java.util.Date;
 
-public class Media {
-	
-	private int id;
-	private boolean isBorrowed;
-	private int year;
-	private String title;
-	private Date borrowDate;
+public abstract class Media {
 
-	public Media(int id, String title, int year, boolean isBorrowed) 
-	{
-		this.id = id;
-		this.title = title;
-		this.year = year;
-		this.isBorrowed = isBorrowed;
-	}
-	
-	public int getId() 
-	{
-		return this.id;
-	}
-	
-	public String getTitle() 
-	{
-		return this.title;
-	}
-	
-	public int getYear()
-	{
-		return this.year;
-	}
-	
-	public boolean isBorrowed() 
-	{
-		return isBorrowed; 
-	}
-	
-	public void setBorrowDate(Date borrowDate)
-	{
-		this.borrowDate = borrowDate;
-	}
-	
-	public Date getBorrowDate()
-	{
-		return this.borrowDate;
-	}
-	
-	@Override
-	public String toString() 
-	{
-		return "Media [id=" + id + ", isBorrowed=" + isBorrowed + ", year=" + year + ", title=" + title
-				+ ", borrowDate=" + borrowDate + "]";
-	}
+    private int id;
+    private boolean isBorrowed;
+    private int year;
+    private String title;
+    private Date borrowDate;
+
+    public Media(int id, String title, int year, boolean isBorrowed) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.isBorrowed = isBorrowed;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowDate(Date borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public Date getBorrowDate() {
+        return this.borrowDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Media [id=" + id + ", isBorrowed=" + isBorrowed + ", year=" + year + ", title=" + title
+                + ", borrowDate=" + borrowDate + "]";
+    }
 }
