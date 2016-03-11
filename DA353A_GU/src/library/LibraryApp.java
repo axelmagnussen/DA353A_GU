f10062491ed2e37eadd6342ca56d62a3fbe4bbe0
Jonas <jonazeiselt@gmail.com>
2016-03-11 12:20:15 +0100

    controller update

--- DA353A_GU/src/library/LibraryApp.java
+++ DA353A_GU/src/library/LibraryApp.java
@@ -2,4 +2,9 @@ package library;
 
 public class LibraryApp {
 
+	public static void showUserPage() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- DA353A_GU/src/library/LibraryController.java
+++ DA353A_GU/src/library/LibraryController.java
@@ -1,12 +1,24 @@
 package library;
 
-public class LibraryController {
+import javax.swing.JOptionPane;
 
-
-	
+public class LibraryController 
+{
+	public LibraryController() {
+		Library.readMedia("src/files/Media.txt");
+		Library.readMembers("src/files/Lantagare.txt");
+	}
 	
 	public void setUserInput(String idNbr) {
-		Library.memberExists(idNbr);
+		if(Library.memberExists(idNbr)) 
+		{
+			JOptionPane.showMessageDialog(null, "User does exist :)", "Info", JOptionPane.INFORMATION_MESSAGE);
+			LibraryApp.showUserPage();
+		}
+		else 
+		{
+			JOptionPane.showMessageDialog(null, "User does not exist! Contact staff or try again.", "Error", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 
 }
