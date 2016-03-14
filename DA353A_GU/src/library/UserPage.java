e2fb2b4b33944ad00a5ad5d0f6cf2d1d60c0a02b
Jonas <jonazeiselt@gmail.com>
2016-03-14 09:50:33 +0100

    UserPage.java comments -> KLAR

--- DA353A_GU/src/library/UserPage.java
+++ DA353A_GU/src/library/UserPage.java
@@ -22,6 +22,10 @@ import collections.AVLTree;
 import media.Media;
 
 /**
+ * This class represents the user's own user page where he/she can 
+ * borrow, return, look up, display media objects. The class uses a MediaTable
+ * to display the borrowed media and a SearchTable to display all media existing
+ * in the library.
  * 
  * @author Ninjakids
  * @since 2016-03-13
@@ -41,7 +45,7 @@ public class UserPage extends JPanel {
 
 	public JTextField mediaIDTxtF = new JTextField();
 
-	public JButton infoBtn = new JButton("Show Detailed Information"); //NYTT
+	public JButton infoBtn = new JButton("Show Detailed Information"); 
 	public JButton borrowBtn = new JButton("Borrow");
 	public JButton returnBtn = new JButton("Return");
 	public JButton signOutBtn = new JButton("Sign Out");
@@ -49,19 +53,21 @@ public class UserPage extends JPanel {
 
 	private MediaTable mediaTable;
 
-	private JScrollPane scroll;
+	private JScrollPane scroll; // Used to encapsulate the MediaTable
 
-	private LibraryMember currentUser;
+	private LibraryMember currentUser; // Current user logged in at the system
 
 	/**
-	 * 
-	 * @param libC
+	 * Constructor that initializes the user page with various components
+	 * such as buttons, tables, a controller, and labels.
+	 * @param libC controller used for controlling the user page
 	 */
 	public UserPage(LibraryController libC) {
 
 		this.controller = libC;
 		currentUser = controller.getCurrentUser();
 
+		// Initializes a MediaTable
 		mediaTable = new MediaTable(currentUser.getBorrowedMedia());
 
 		this.setLayout(new BorderLayout());
@@ -77,18 +83,20 @@ public class UserPage extends JPanel {
 
 		mediaIDTxtF.setColumns(20);
 
-		JPanel inputPanel = new JPanel();
+		// Panel with "Enter Media-ID"-label and text field
+		JPanel inputPanel = new JPanel(); 
 		inputPanel.add(txtFlbl);
 		inputPanel.add(Box.createRigidArea(new Dimension(25,0))); // Adds empty space in inputPanel
 		inputPanel.add(mediaIDTxtF);
 
+		// "Show Detailed Info"
 		JPanel infoPanel = new JPanel();
-		infoPanel.add(infoBtn); //NYTT
+		infoPanel.add(infoBtn); 
 
-		JPanel upperPanel = new JPanel(new GridLayout(3,1,0,20));
+		JPanel upperPanel = new JPanel(new GridLayout(3,1,0,20)); // rows, cols, hGap, vGap
 		upperPanel.add(welcomeXlbl);
 		upperPanel.add(inputPanel);
-		upperPanel.add(infoPanel); //NYTT
+		upperPanel.add(infoPanel);
 
 		this.addButtonListeners(); // layout + button listeners
 		
@@ -109,14 +117,19 @@ public class UserPage extends JPanel {
 		
 		this.add(centerPanel, BorderLayout.CENTER);
 		
-		mediaTable.getSelectionModel().addListSelectionListener(new SelectionListener());
+		mediaTable.getSelectionModel().addListSelectionListener(new SelectionListener()); // adds action-listener to MediaTable
 	}
 
+	/**
+	 * Method that sets button sizes but its main task is adding button listeners 
+	 * to the buttons. Lambda expressions are used for adding actionListeners.
+	 */
 	private void addButtonListeners() {
 
 		infoBtn.setPreferredSize(new Dimension(540, 35));
 		infoBtn.addActionListener(e -> {	
 			try {
+				// trim used for deleting any leading and trailing white spaces
 				controller.showDetailedInfo(Integer.parseInt(mediaIDTxtF.getText().trim()));
 
 			} catch(NumberFormatException ex) {
@@ -159,8 +172,8 @@ public class UserPage extends JPanel {
 	}
 
 	/**
-	 * 
-	 * @return
+	 * Method that returns a panel with buttons in it.
+	 * @return JPanel with buttons
 	 */
 	private JPanel getButtonPanel() {
 
@@ -177,29 +190,28 @@ public class UserPage extends JPanel {
 	}
 
 	/**
-	 * 
+	 * This class is used for getting values from the MediaTable (JTable)
+	 * when user clicks on a certain row. Whenever user clicks on a certain 
+	 * row and that row does have values, an idNbr is set in the text-field
+	 * mediaIDTxtF.
+	 *  
 	 * @author Ninjakids
 	 * @since 2016-03-13
 	 */
-	private class SelectionListener implements ListSelectionListener 
-	{
-		public void valueChanged(ListSelectionEvent e) 
-		{
-			if (e.getValueIsAdjusting()) 
-			{
+	private class SelectionListener implements ListSelectionListener {
+		
+		public void valueChanged(ListSelectionEvent e) {
+			
+			if (e.getValueIsAdjusting()) {
 				return; // mouse button not released yet
 			}
 			int row = mediaTable.getSelectedRow();
-			if (row < 0) 
-			{
+			if (row < 0) {
 				return; // true when clearSelection
 			}
-			System.out.println("Selected row: " + row);
-			Media selMedia = getSelectedMedia(row);
-			System.out.println("Selected media: " + selMedia);
+			Media selMedia = getSelectedMedia(row); // What media was clicked?
 
-			if(selMedia != null)
-			{
+			if(selMedia != null) {
 				mediaIDTxtF.setText(selMedia.getId() + "");
 			}
 			mediaTable.clearSelection();
@@ -207,26 +219,24 @@ public class UserPage extends JPanel {
 	}
 
 	/**
-	 * 
+	 * Method that returns media object associated with the value of row. An
+	 * iterator (in order) is used for traversing, updating a local variable 
+	 * index which will in the end return the correct media object.  
 	 * @param row
-	 * @return
+	 * @return media object associated with row
 	 */
-	private Media getSelectedMedia(int row) 
-	{
+	private Media getSelectedMedia(int row) {
+		
 		AVLTree<Date, Media> media = currentUser.getBorrowedMedia();
-		if(media != null) 
-		{
+		if(media != null) {
 			Iterator<Media> it = media.iterator();
 			int index = 0;
-			while(it.hasNext()) 
-			{
+			while(it.hasNext()) {
 				Media current = it.next();
-				if(index == row) 
-				{
+				if(index == row) {
 					return current;
 				} 
-				else 
-				{
+				else {
 					index++;
 				}
 			}
